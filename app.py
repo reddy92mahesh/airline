2821892540af92e1caa93a13738e2f49b471a1a7
@Reddy_Mahesh29 <95978017+reddy92mahesh@users.noreply.github.com>
2026-01-20 17:58:46 +0530

    app.py
    add interactive visualization

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 st.set_page_config(page_title="Airline App", layout="wide")
 
@@ -10,5 +12,18 @@ def load_data():
 df = load_data()
 
 st.title("✈️ Airline Customer Data")
+
 st.subheader("Data Preview")
 st.dataframe(df.head())
+
+# Sidebar
+st.sidebar.header("Visualization")
+
+numeric_cols = df.select_dtypes(include=["int64", "float64"]).columns
+selected_col = st.sidebar.selectbox("Select a column", numeric_cols)
+
+st.subheader(f"Distribution of {selected_col}")
+
+fig, ax = plt.subplots()
+sns.histplot(df[selected_col], kde=True, ax=ax)
+st.pyplot(fig)
